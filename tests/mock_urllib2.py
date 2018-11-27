3ecbc5782d9209b38be2f8b6ded3c8d30b31f517
Cedric Duriau <duriau.cedric@live.be>
2018-11-27 01:51:13 +0100

    mock urllib2 url open

---
+++ tests/mock_urllib2.py
@@ -0,0 +1,19 @@
+# stdlib modules
+import urllib2
+import StringIO
+
+
+def mock_response(req):
+    url = req.get_full_url()
+    if url == "http://valid.com":
+        resp = urllib2.addinfourl(StringIO.StringIO("valid"), "valid", url)
+        resp.code = 200
+        resp.msg = "OK"
+        return resp
+    elif req.get_full_url() == "http://invalid.com":
+        raise urllib2.HTTPError(url, 404, "invalid", "", StringIO.StringIO())
+
+
+class MockHTTPHandler(urllib2.HTTPHandler):
+    def http_open(self, req):
+        return mock_response(req)
