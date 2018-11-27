e498e410607433a284fa8088bc55008f2e34d8c9
Cedric Duriau <duriau.cedric@live.be>
2018-11-27 02:13:13 +0100

    mock specific valid and invalid url version

--- tests/mock_urllib2.py
+++ tests/mock_urllib2.py
@@ -5,12 +5,12 @@ import StringIO
 
 def mock_response(req):
     url = req.get_full_url()
-    if url == "http://valid.com":
+    if url == "http://valid.com/1.0.0":
         resp = urllib2.addinfourl(StringIO.StringIO("valid"), "valid", url)
         resp.code = 200
         resp.msg = "OK"
         return resp
-    elif req.get_full_url() == "http://invalid.com":
+    elif url == "http://invalid.com/1.0.0":
         raise urllib2.HTTPError(url, 404, "invalid", "", StringIO.StringIO())
 
 
