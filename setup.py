4091f7b93243cd663b6b3c74f4ed12a329e01514
Cedric Duriau <duriau.cedric@live.be>
2018-12-15 16:55:03 +0100

    create setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+# stdlib modules
+from setuptools import setup
+
+
+setup(name="PackagerBuddy",
+      version="0.1.0",
+      description="JSON config based software packager.",
+      license="MIT",
+      author="C&eacute;dric Duriau",
+      author_email="duriau.cedric@live.be",
+      url="https://github.com/cedricduriau/PackagerBuddy",
+      packages=["packagerbuddy"],
+      scripts=["bin/packagerbuddy"])
