8a61f1b576121262a30da47dbb75b300a8aa9d2d
Cedric Duriau <duriau.cedric@live.be>
2019-02-15 21:26:15 +0100

    name to lowercase; bump up version; remove setup files

--- setup.py
+++ setup.py
@@ -1,25 +1,13 @@
 # stdlib modules
-import os
 from setuptools import setup
 
 
-def get_default_config():
-    """
-    Gets the default configuration file that ship with the repository.
-
-    :rtype: str
-    """
-    path = os.path.join(os.path.dirname(__file__), "config", "software.json")
-    return os.path.abspath(path)
-
-
-setup(name="PackagerBuddy",
-      version="1.0.1",
+setup(name="packagerbuddy",
+      version="1.1.0",
       description="JSON config based software packager.",
       license="MIT",
       author="C&eacute;dric Duriau",
       author_email="duriau.cedric@live.be",
       url="https://github.com/cedricduriau/PackagerBuddy",
       packages=["packagerbuddy"],
-      scripts=["bin/packagerbuddy"],
-      data_files=[("config", [get_default_config()])])
+      scripts=["bin/packagerbuddy"])
