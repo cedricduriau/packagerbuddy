5bb31cfed505ae7b1c77df3773be192231d78899
Cedric Duriau <duriau.cedric@live.be>
2019-03-10 23:47:18 +0100

    refactor; add response mock for filename test

--- tests/mock_urllib.py
+++ tests/mock_urllib.py
@@ -11,12 +11,20 @@ except ImportError:
 
 def mock_response(req):
     url = req.get_full_url()
-    if url.startswith("http://valid.com"):
+
+    if url.startswith("http://valid"):
         resp = addinfourl(StringIO("valid"), "valid", url)
         resp.code = 200
         resp.msg = "OK"
+        resp.headers = {"content-disposition": "filename=valid.tar"}
         return resp
-    elif url.startswith("http://invalid.com"):
+    elif url.startswith("http://filename"):
+        resp = addinfourl(StringIO("filename"), "filename", url)
+        resp.code = 200
+        resp.msg = "OK"
+        resp.headers = {}
+        return resp
+    elif url.startswith("http://invalid"):
         raise HTTPError(url, 404, "invalid", "", StringIO())
 
 
