8c5697960bf637e5eac4c5c8ae2d811020e4e75e
Cedric Duriau <duriau.cedric@live.be>
2019-03-11 00:24:03 +0100

    make repo name lowercase; add dirs to create

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(name="packagerbuddy",
       license="MIT",
       author="C&eacute;dric Duriau",
       author_email="duriau.cedric@live.be",
-      url="https://github.com/cedricduriau/PackagerBuddy",
+      url="https://github.com/cedricduriau/packagerbuddy",
       packages=["packagerbuddy"],
-      scripts=["bin/packagerbuddy"])
+      scripts=["bin/packagerbuddy"],
+      data_files=[("~/.packagerbuddy/source", []),
+                  ("~/.packagerbuddy/installed", []),
+                  ("~/.packagerbuddy/config", []),
+                  ("~/.packagerbuddy/scripts", [])])
