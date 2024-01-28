c34f9bb69212c683f76056868b0d917e921b2222
Cédric Duriau <cedric.duriau@deliverect.com>
2024-01-28 15:36:33 +0100

    lint

--- tests/test_archiveutils.py
+++ tests/test_archiveutils.py
@@ -13,12 +13,15 @@ def test_build_install_path(mock_settings_dir_install: None) -> None:
     archiveutils.build_install_path("foo", "bar") == "/tmp/installed/foo-bar"
 
 
-@pytest.mark.parametrize(["path", "expected_root", "expected_ext"], [
-    ("/root/dir/file.zip", "/root/dir/file", ".zip"),
-    ("/root/dir/file.tar", "/root/dir/file", ".tar"),
-    ("/root/dir/file.tar.gz", "/root/dir/file", ".tar.gz"),
-    ("/root/dir/file.dot1.dot2.tar.gz", "/root/dir/file.dot1.dot2", ".tar.gz"),
-])
+@pytest.mark.parametrize(
+    ["path", "expected_root", "expected_ext"],
+    [
+        ("/root/dir/file.zip", "/root/dir/file", ".zip"),
+        ("/root/dir/file.tar", "/root/dir/file", ".tar"),
+        ("/root/dir/file.tar.gz", "/root/dir/file", ".tar.gz"),
+        ("/root/dir/file.dot1.dot2.tar.gz", "/root/dir/file.dot1.dot2", ".tar.gz"),
+    ],
+)
 def test_split_ext(path: str, expected_root: str, expected_ext: str) -> None:
     root, ext = archiveutils.split_ext(path)
     assert root == expected_root
